585ba7a96dff37d6c0a0eacd790a56dee5db5d3b
Shaik Mohammad Sameer <skmdsameer4329@gmail.com>
2026-07-18 15:06:17 +0530

    Add files via upload
    A simple command-line calculator built using Python. This project performs addition, subtraction, multiplication, and division. It also handles invalid operators and division by zero. Created as part of my Python learning journey and GitHub portfolio.

---
+++ calculator.py
@@ -0,0 +1,19 @@
+print("===== Simple Calculator =====")
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print("Answer =", num1 + num2)
+elif op == "-":
+    print("Answer =", num1 - num2)
+elif op == "*":
+    print("Answer =", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Answer =", num1 / num2)
+    else:
+        print("Cannot divide by zero!")
+else:
+    print("Invalid operator")
